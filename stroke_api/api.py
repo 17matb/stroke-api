4748e1e3b8a282642fe0597e1a48e342db702074
17matb <147427088+17matb@users.noreply.github.com>
2025-08-07 15:38:04 +0200

    implemented patient retrieval endpoints and added statistics route; enhanced filter functions for gender, stroke status, and patient ID

--- stroke_api/api.py
+++ stroke_api/api.py
@@ -8,14 +8,28 @@ def read_root():
     return {"message": "Bienvenue sur l'API Stroke Prediction !"}
 
 # TODO décommenter et compléter
-#@router.get("/patients/")
-#def get_patients(gender: str = None, stroke: int = None, max_age: float = None):
-    #filtered_df = 
-    #return filtered_df.to_dict(orient="records")
+@router.get("/patients/")
+def get_patients(gender: str = None, stroke: int = None, max_age: float = None):
+    result = filters.filter_patient(gender=gender, stroke=stroke, max_age=max_age)
+    return result
 
 # TODO décommenter et compléter
-#@router.get("/patients/{patient_id}")
-    # Gérer le cas où l'id de patient passé en paramètre n'existe pas
-
+@router.get("/patients/{patient_id}")
+def get_patients_by_id(patient_id: int = None):
+    result = filters.filter_patient_by_id(patient_id)
+    return result
+# Gérer le cas où l'id de patient passé en paramètre n'existe pas
 
 # TODO Ajout de la route stats
+@router.get('/stats/')
+def get_stats():
+    # Fournit des statistiques agrégées sur les patients (ex. : nb total de patients, âge moyen, taux d’AVC, répartition hommes/femmes).
+    df = filters.stroke_data.copy()
+
+    output = {
+        'total_patients': int(df['id'].count()),
+        'average_age': float(df['age'].mean().round(2)),
+        'stroke_rate': float(df['stroke'].mean().round(2)),
+        'gender_rate': {i: float((((df['gender'].loc[df['gender'] == i].count())/df['gender'].count())*100).round(2)) for i in df['gender'].unique()}
+    }
+    return output
--- stroke_api/filters.py
+++ stroke_api/filters.py
@@ -1,7 +1,36 @@
-from typing import Optional
+from typing import Literal
 import pandas as pd
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
 
 # Chargement des données (une fois)
+stroke_data = pd.read_parquet('./data/data.parquet')
+
+@app.get('/patients/')
+def filter_patient(gender: Literal['male', 'female', 'other'] = None, stroke: Literal[0,1] = None, max_age: float = None) -> list[dict]:
+    df = stroke_data.copy()
+    if gender is not None:
+        df = df.loc[df['gender'] == gender]
+    if stroke is not None:
+        df = df.loc[df['stroke'] == stroke]
+    if max_age is not None:
+        df = df.loc[df['age'] <= max_age]
+
+    result = df.to_dict('records')
+    return result
+
+@app.get('/patients/{patient_id}')
+def read_patient(patient_id):
+    return {'patient_id': patient_id}
+
+def filter_patient_by_id(patient_id):
+    df = stroke_data.copy()
+    df = df.loc[df['id'] == patient_id]
+    if df.empty:
+        raise HTTPException(404, 'Patient ID not found')
+    else:
+        return df.to_dict('records')
 
 # Tester l'app avec :
 # poetry run fastapi dev stroke_api/main.py
